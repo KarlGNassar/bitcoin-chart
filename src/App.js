e993423af3485a8d74563d68b97507efb8a4d17f
KARL NASSAR <kgnassar01@gmail.com>
2021-03-03 01:51:00 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, { useEffect, useState, useMemo } from 'react'
+ import Chart from 'react-apexcharts'
+
+const proxyUrl = 'http://cors-anywere.herokuapp.com/'
+const stockUrl = `${proxyUrl}https://query1.finance.yahoo.com/v8/finance/chart/BTC-USD?region=US&lang=en-US&includePrePost=false&interval=1mo&range=max&corsDomain=uk.search.yahoo.com`
+async function getStocks() {
+  const response = await fetch(stockUrl)
+  return response.json()
+}
+
+const chart = {
+  options: {
+    chart: {
+      type: 'candlestick',
+      height: 350
+    },
+    title: {
+      text: 'Bitcoin Chart',
+      align: 'left'
+    },
+    xaxis: {
+      type: 'datetime'
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true
+      }
+    }
+  },
+}
+
+const round = (number) => {
+  return number ? +(number.toFixed(2)) : null
+}
 
 function App() {
+  const [series, setSeries] = useState([{
+    data: []
+  }])
+  const [price, setPrice] = useState(-1)
+  const [prevPrice, setPrevPrice] = useState(-1)
+  const [priceTime, setPriceTime] = useState(null)
+
+  useEffect(() => {
+    let timeoutId
+    async function getLatestPrice() {
+      try {
+        const data = await getStocks()
+        const btc = data.chart.result[0]
+        setPrevPrice(price)
+        setPrice(btc.meta.regularMarketPrice.toFixed(2))
+        setPriceTime(new Date(btc.meta.regularMarketTime * 1000))
+        const quote = btc.indicators.quote[0]
+        const prices = btc.timestamp.map((timestamp, index) => ({
+          x: new Date(timestamp * 1000),
+          y: [quote.open[index], quote.high[index], quote.low[index], quote.close[index]].map(round)
+        }))
+        setSeries([{
+          data: prices,
+        }])
+      } catch (error) {
+        console.log(error)
+      }
+      timeoutId = setTimeout(getLatestPrice, 10000)
+    }
+    
+    getLatestPrice()
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [price])
+
+  const direction = useMemo(() =>
+    prevPrice < price ? 'up' : prevPrice > price ? 'down' : '',
+  [])
+
+  const directionEmojies = {
+    'up': '📈',
+    'down': '📉',
+    '': '',
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="ticker">
+        BTC
+      </div>
+
+      <div className={['price', direction].join(' ')}>
+        ${price} {directionEmojies[direction]}
+      </div>
+
+      <div className="price-time">
+        {priceTime && priceTime.toLocaleTimeString()}
+      </div>
+
+      <Chart
+        options={chart.options}
+        series={series}
+        type="candlestick"
+        width="100%"
+        height={320}
+      />
     </div>
   );
 }
