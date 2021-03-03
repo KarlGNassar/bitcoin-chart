e9d3180f4e024f176cf30482da68a035e5782914
KARL NASSAR <kgnassar01@gmail.com>
2021-03-03 15:02:19 +0200

    api fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
  import React, { useEffect, useState, useMemo } from 'react'
  import Chart from 'react-apexcharts'
 
-const proxyUrl = 'http://cors-anywere.herokuapp.com/'
-const stockUrl = `https://query1.finance.yahoo.com/v8/finance/chart/BTC-USD?region=US&lang=en-US&includePrePost=false&interval=1mo&range=max&corsDomain=uk.search.yahoo.com`
-async function getStocks() {
-  const response = await fetch(stockUrl)
+const priceUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+const ohlcUrl = 'https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=30'
+async function getOpenHighLowClose() {
+  const response = await fetch(ohlcUrl)
+  return response.json()
+}
+async function getPrice() {
+  const response = await fetch(priceUrl)
   return response.json()
 }
 
 const chart = {
   options: {
+    theme: {
+      mode: 'dark',
+    },
     chart: {
       type: 'candlestick',
       height: 350
@@ -23,14 +30,11 @@ const chart = {
     },
     yaxis: {
       tooltip: {
-        enabled: true
+        enabled: true,
+        theme: 'dark',
       }
     }
   },
-}
-
-const round = (number) => {
-  return number ? +(number.toFixed(2)) : null
 }
 
 function App() {
@@ -39,21 +43,18 @@ function App() {
   }])
   const [price, setPrice] = useState(-1)
   const [prevPrice, setPrevPrice] = useState(-1)
-  const [priceTime, setPriceTime] = useState(null)
 
   useEffect(() => {
     let timeoutId
     async function getLatestPrice() {
       try {
-        const data = await getStocks()
-        const btc = data.chart.result[0]
+        const data = await getPrice()
+        const ohlc = await getOpenHighLowClose()
         setPrevPrice(price)
-        setPrice(btc.meta.regularMarketPrice.toFixed(2))
-        setPriceTime(new Date(btc.meta.regularMarketTime * 1000))
-        const quote = btc.indicators.quote[0]
-        const prices = btc.timestamp.map((timestamp, index) => ({
-          x: new Date(timestamp * 1000),
-          y: [quote.open[index], quote.high[index], quote.low[index], quote.close[index]].map(round)
+        setPrice(data.bitcoin.usd)
+        const prices = ohlc.map((timestamp, index) => ({
+          x: new Date(ohlc[index][0]),
+          y: [ohlc[index][1], ohlc[index][2], ohlc[index][3], ohlc[index][4]],
         }))
         setSeries([{
           data: prices,
@@ -72,36 +73,36 @@ function App() {
   }, [price])
 
   const direction = useMemo(() =>
-    prevPrice < price ? 'up' : prevPrice > price ? 'down' : '',[price, prevPrice])
+    prevPrice < price ? 'up' : prevPrice > price ? 'down' : ' ',[price, prevPrice])
 
   const directionEmojies = {
     'up': '📈',
     'down': '📉',
-    '': '',
+    ' ': ' ',
   }
 
 
   return (
     <div className="container">
-      <div className="ticker">
-        BTC
+      <div className="header">
+        <div className="ticker">
+          BTC
+        </div>
+
+        <div className={['price', direction].join(' ')}>
+          ${price} {directionEmojies[direction]}
+        </div>
       </div>
 
-      <div className={['price', direction].join(' ')}>
-        ${price} {directionEmojies[direction]}
+      <div className="chart">
+        <Chart
+          options={chart.options}
+          series={series}
+          type="candlestick"
+          width="100%"
+          height={450}
+        />
       </div>
-
-      <div className="price-time">
-        {priceTime && priceTime.toLocaleTimeString()}
-      </div>
-
-      <Chart
-        options={chart.options}
-        series={series}
-        type="candlestick"
-        width="100%"
-        height={320}
-      />
     </div>
   );
 }
